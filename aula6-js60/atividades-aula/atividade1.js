d1e56d79316b0c486efede46ef64032d0b509bbc
João Wiltenburg <158215503+jotaPwt@users.noreply.github.com>
2026-02-11 21:56:46 -0300

    Add files via upload

---
+++ aula6-js60/atividades-aula/atividade1.js
@@ -0,0 +1,18 @@
+// **ATIVIDADE 1:**
+
+// ***hello world -  com Javascript***
+
+// console.log() -  Imprime no ambiente de teste do navegador.
+
+// alert() -  Alerta no navegador.
+
+// document.write  - document.write():grava conteúdo HTML para documento no navegador
+
+console.log('Hello World')
+
+
+alert("Alerta no navegador")
+
+
+let teste = 'teste'
+document.write(teste)
---
+++ aula6-js60/aula6/script.js
@@ -0,0 +1,17 @@
+console.log('Hello world')
+
+
+let variavel = "muda"
+const constante = "naomuda"
+
+console.log(variavel, constante)
+
+
+let peso = document.getElementById('peso')
+let altura = document.getElementById("altura")
+
+let calcular = document.getElementById('calcular')
+
+calcular ==
+
+console.log(peso,altura)
